6a185f9cdce5e84eb43d66db25cc968848697caf
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2012-12-22 22:24:49 +0100

    Update to django1.4 settings.

--- easy_maps_tests/settings.py
+++ easy_maps_tests/settings.py
@@ -3,12 +3,26 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.abspath(os.path.join(PROJECT_ROOT, '..')))
 
 DEBUG = True
-DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME = 'db.sqlite'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite',
+    }
+}
 ROOT_URLCONF = 'urls'
 TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, 'templates'),)
 
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.abspath(os.path.join(PROJECT_ROOT, 'static'))
+
 INSTALLED_APPS=(
+    'django.contrib.contenttypes',
+    'django.contrib.messages',
+    'django.contrib.sessions',
+    'django.contrib.auth',
+    'django.contrib.admin',
+    'django.contrib.staticfiles',
     'easy_maps',
     'test_app',
 #    'devserver',
