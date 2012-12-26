5f9eed927d4659393b4ac01a7313fc6021ed6f9e
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2012-12-26 20:55:03 +0100

    Add example models.

--- easy_maps_tests/test_app/models.py
+++ easy_maps_tests/test_app/models.py
@@ -1,1 +1,27 @@
 #hello, testrunner!
+from django.db import models
+from django.contrib import admin
+
+from easy_maps.models import Address
+from easy_maps.admin import AddressInlineAdmin
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+
+    def count(self):
+        return self.shop_set.count()
+
+class Shop(Address):
+    brand = models.ForeignKey(Brand)
+
+class ShopInlineAdmin(AddressInlineAdmin):
+    model = Shop
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['name', 'count',]
+    model = Brand
+    inlines = [
+        ShopInlineAdmin,
+    ]
+
+admin.site.register(Brand, BrandAdmin)
