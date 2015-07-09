1bfb2fd753c02507a709f5601ea7269b3f729772
bashu <basil.shubin@gmail.com>
2015-07-09 12:14:44 +0600

    updating tests

--- easy_maps/tests.py
+++ easy_maps/tests.py
@@ -17,16 +17,16 @@ from .models import Address
 
 
 class AddressTests(TestCase):
-    fake_default_center  = (1, 2)
-
-    html = """{%% load easy_maps_tags %%}{%% easy_map "%(address)s" 500 500 10 %%}"""
+    fake_default_center = (1, 2)
 
     def test_empty_dont_save_on_db(self):
         # If we pass an empty address we don't save nothing in the
         # database
+        html = """{%% load easy_maps_tags %%}{%% easy_map '%(varname)s' 500 500 10 %%}"""
+    
         before_count = len(Address.objects.all())
 
-        t = template.Template(self.html % {"address": ""})
+        t = template.Template(html % {"varname": ""})
         t.render(template.Context({}))
 
         after_count = len(Address.objects.all())
@@ -56,46 +56,44 @@ class AddressTests(TestCase):
     #     self.assertEqual(address[0].latitude, AddressTests.fake_default_center[0])
     #     self.assertEqual(address[0].longitude, AddressTests.fake_default_center[1])
 
-    # @override_settings(EASY_MAPS_CENTER=fake_default_center)
-    # def test_normal_address(self):
-    #     """If we pass an address don't use the defaults"""
-    #     n_addresses_before = len(Address.objects.all())
+    @override_settings(EASY_MAPS_CENTER=fake_default_center)
+    def test_normal_address(self):
+        # If we pass an address don't use the defaults
+        html = """{%% load easy_maps_tags %%}{%% easy_map '%(varname)s' 500 500 10 %%}"""
 
-    #     a = "Ekaterinburg, Mira 33"
-    #     simple_template_string = """{%% load easy_maps_tags %%}
-    #     {%% easy_map "%s" 500 500 10 %%}
-    #     """ % a
-    #     t = template.Template(simple_template_string)
-    #     t.render(template.Context({}))
+        before = len(Address.objects.all())
 
-    #     address = Address.objects.get(address=a)
-    #     self.assertNotEqual(address.latitude, AddressTests.fake_default_center[0])
-    #     self.assertNotEqual(address.longitude, AddressTests.fake_default_center[1])
+        a = "Ekaterinburg, Mira 33"
+        
+        t = template.Template(html % {"varname": a})
+        t.render(template.Context({}))
 
-    #     n_addresses_after = len(Address.objects.all())
+        address = Address.objects.get(address=a)
 
-    #     self.assertEqual(n_addresses_after, n_addresses_before + 1)
+        self.assertNotEqual(address.latitude, AddressTests.fake_default_center[0])
+        self.assertNotEqual(address.longitude, AddressTests.fake_default_center[1])
 
-    # @override_settings(EASY_MAPS_CENTER=fake_default_center)
-    # def test_use_address_instance(self):
-    #     """It's possible to pass directly to the easy_map tag an Address instance.
+        after = len(Address.objects.all())
 
-    #     This test checks also that the database is not hit.
-    #     """
-    #     # create a fake address
-    #     Address.objects.create(address='fake')
-    #     n_addresses_before = len(Address.objects.all())
+        self.assertEqual(after, before + 1)
 
-    #     simple_template_string = """{% load easy_maps_tags %}
-    #     {% easy_map address 500 500 10 %}
-    #     """
-    #     address = Address.objects.all()[0]
-    #     t = template.Template(simple_template_string)
-    #     # this assert works only from django1.3
-    #     ctx = template.Context({'address': address,})
-    #     self.assertNumQueries(0, lambda: t.render(ctx))
+    @override_settings(EASY_MAPS_CENTER=fake_default_center)
+    def test_use_address_instance(self):
+        # It's possible to pass directly to the easy_map tag an Address instance.
+        # This test checks also that the database is not hit.
+        html = """{%% load easy_maps_tags %%}{%% easy_map %(varname)s 500 500 10 %%}"""
 
-    #     n_addresses_after = len(Address.objects.all())
+        # create a fake address
+        a = Address.objects.create(address='fake')
 
-    #     # no Address is created in the process
-    #     self.assertEqual(n_addresses_after, n_addresses_before)
+        before = len(Address.objects.all())
+
+        t = template.Template(html % {'varname': 'address'})
+        ctx = template.Context({'varname': a})
+
+        self.assertNumQueries(0, lambda: t.render(ctx))
+
+        after = len(Address.objects.all())
+
+        # no new records are created in the process
+        self.assertEqual(after, before)
