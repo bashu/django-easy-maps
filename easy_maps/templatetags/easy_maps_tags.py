b4d2dfa5c0a5be8c7f8d14a7d65a9b849472ccb0
bashu <basil.shubin@gmail.com>
2016-10-10 16:07:16 +0700

    better ``TemplateSyntaxError`` exception handling

--- easy_maps/templatetags/easy_maps_tags.py
+++ easy_maps/templatetags/easy_maps_tags.py
@@ -11,8 +11,6 @@ from ..models import Address
 
 register = template.Library()
 
-CENTER = settings.EASY_MAPS_CENTER
-
 
 class EasyMapTag(InclusionTag):
     """
@@ -31,14 +29,14 @@ class EasyMapTag(InclusionTag):
         Argument('address', resolve=True, required=True),
         IntegerArgument('width', required=False, default=None),
         IntegerArgument('height', required=False, default=None),
-        IntegerArgument('zoom', required=False, default=16),
+        IntegerArgument('zoom', required=False, default=None),
         'using',
         Argument('template_name', default=None, required=False),
     )
 
     def render_tag(self, context, **kwargs):
-        params = dict((k, v) for k, v in kwargs.items() if v is not None)
-        if len(params.keys()) == 3 or len(params.keys()) > 5:
+        params = dict((k, v) for k, v in kwargs.items() if v and k not in ['template_name'])
+        if 'address' in params and (len(params) == 2 or len(params) > 4):
             raise template.TemplateSyntaxError(
                 "easy_map tag has the following syntax: "
                 "{% easy_map <address> [<width> <height>] [zoom] [using <template_name>] %}"
@@ -53,7 +51,10 @@ class EasyMapTag(InclusionTag):
             return address
 
         if not address:
-            return Address(latitude=CENTER[0], longitude=CENTER[1])
+            return Address(
+                latitude=settings.EASY_MAPS_CENTER[0],
+                longitude=settings.EASY_MAPS_CENTER[1],
+            )
         else:
             return Address.objects.get_or_create(address=address)[0]
 
@@ -61,6 +62,8 @@ class EasyMapTag(InclusionTag):
 
     def get_context(self, context, **kwargs):
         kwargs.update({'map': self.parse_address(kwargs.pop('address'))})
+        if not kwargs.get('zoom', None):
+            kwargs['zoom'] = 16  # default value
         return kwargs
 
 register.tag(EasyMapTag)
