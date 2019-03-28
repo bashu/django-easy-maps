5be6ce4697cb60ab81074e4fd480296d6b416ec9
Basil Shubin <basil.shubin@gmail.com>
2019-03-28 20:37:34 +0700

    close #67

--- easy_maps/tests.py
+++ easy_maps/tests.py
@@ -1,20 +1,12 @@
 # -*- coding: utf-8 -*-
 
-# import mock
+import mock
 
 from django import template
 from django.test import TestCase
+from django.test.utils import override_settings
 
-try:
-    from django.test.utils import override_settings
-except ImportError:
-    from django.utils import unittest
-
-    def override_settings(*args, **kwargs):
-        return unittest.skip(
-            "overriding settings is not supported by this django version")
-
-from easy_maps.models import Address
+from .models import Address
 
 
 class AddressTests(TestCase):
@@ -25,45 +17,46 @@ class AddressTests(TestCase):
         # database
         html = "{%% load easy_maps_tags %%}{%% easy_map '%(v)s' 500 500 10 %%}"
 
-        before_count = len(Address.objects.all())
+        n_addresses_before = len(Address.objects.all())
 
         t = template.Template(html % {"v": ""})
         t.render(template.Context({}))
 
-        after_count = len(Address.objects.all())
+        n_addresses_after = len(Address.objects.all())
 
-        self.assertEqual(after_count, before_count)
+        self.assertEqual(n_addresses_after, n_addresses_before)
 
-    # @override_settings(EASY_MAPS_CENTER=fake_default_center)
-    # def test_empty_address_use_defaults(self):
-    #     # When an empty address is passed uses the EASY_MAPS_CENTER
-    #     # setting
-    #     html = "{%% load easy_maps_tags %%}{%% easy_map '%(v)s' 500 500 10 %%}"
+    @override_settings(EASY_MAPS_CENTER=fake_default_center)
+    def test_empty_address_use_defaults(self):
+        # When an empty address is passed uses the EASY_MAPS_CENTER
+        # setting
+        html = "{%% load easy_maps_tags %%}{%% easy_map '%(v)s' 500 500 10 %%}"
 
-    #     address = [None]  # nonlocal
+        address = [None]  # nonlocal
 
-    #     # below we patch the render_to_string in order to retrieve the
-    #     # map context variable and check its coordinate
-    #     def get_map_context_instance(*args, **kwargs):
-    #         address[0] = kwargs['context_instance']['map']
-    #         return ''
+        # below we patch the render_to_string in order to retrieve the
+        # map context variable and check its coordinate
+        def get_address_instance(*args, **kwargs):
+            template_name, context = args
+            address[0] = context['map']
+            return ''
 
-    #     t = template.Template(html % {"varname": ""})
-    #     with mock.patch(
-    #             'easy_maps.templatetags.easy_maps_tags.render_to_string', get_map_context_instance):
-    #         t.render(template.Context({}))
+        t = template.Template(html % {"v": ""})
+        with mock.patch(
+                'classytags.helpers.render_to_string', get_address_instance):
+            t.render(template.Context({}))
 
-    #     self.assertEqual(
-    #         address[0].latitude, AddressTests.fake_default_center[0])
-    #     self.assertEqual(
-    #         address[0].longitude, AddressTests.fake_default_center[1])
+        self.assertEqual(
+            address[0].latitude, AddressTests.fake_default_center[0])
+        self.assertEqual(
+            address[0].longitude, AddressTests.fake_default_center[1])
 
     @override_settings(EASY_MAPS_CENTER=fake_default_center)
     def test_normal_address(self):
         # If we pass an address don't use the defaults
         html = "{%% load easy_maps_tags %%}{%% easy_map '%(v)s' 500 500 10 %%}"
 
-        before = len(Address.objects.all())
+        n_addresses_before = len(Address.objects.all())
 
         a = "Ekaterinburg, Mira 33"
 
@@ -77,9 +70,9 @@ class AddressTests(TestCase):
         self.assertNotEqual(
             address.longitude, AddressTests.fake_default_center[1])
 
-        after = len(Address.objects.all())
+        n_addresses_after = len(Address.objects.all())
 
-        self.assertEqual(after, before + 1)
+        self.assertEqual(n_addresses_after, n_addresses_before + 1)
 
     @override_settings(EASY_MAPS_CENTER=fake_default_center)
     def test_use_address_instance(self):
@@ -91,14 +84,14 @@ class AddressTests(TestCase):
         # create a fake address
         a = Address.objects.create(address='fake')
 
-        before = len(Address.objects.all())
+        n_addresses_before = len(Address.objects.all())
 
         t = template.Template(html % {'v': 'address'})
         ctx = template.Context({'v': a})
 
         self.assertNumQueries(0, lambda: t.render(ctx))
 
-        after = len(Address.objects.all())
+        n_addresses_after = len(Address.objects.all())
 
-        # no new records are created in the process
-        self.assertEqual(after, before)
+        # no address is created in the process
+        self.assertEqual(n_addresses_after, n_addresses_before)
