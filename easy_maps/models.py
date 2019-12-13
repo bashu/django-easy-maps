e9beeb7478cd5bbad265653f704add29e8f5bb9c
Denis Kopitsa <denis.kopitsa@gmail.com>
2019-12-13 13:15:49 +0300

    add Django 3.0 support
    move `python_2_unicode_compatible` to external six

--- easy_maps/models.py
+++ easy_maps/models.py
@@ -7,7 +7,7 @@ from datetime import timedelta
 
 from django.db import models
 from django.utils import timezone
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 from . import geocode
